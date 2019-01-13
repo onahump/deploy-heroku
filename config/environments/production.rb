c99b31f83fd03131764b78affb0b3d614eeecf93
onahump <oswaldo@makingdevs.com>
2019-01-13 11:47:21 -0600

    Adding configuration to production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,13 +82,15 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  # config/environments/production.rb
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
-
+  
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
